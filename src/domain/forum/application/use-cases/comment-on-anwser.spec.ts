a498c1aca6257c939c6f1495dfffc8884077b03a
Eduardo Soares <eduardoosooares@gmail.com>
2025-12-29 09:06:18 -0300

    feat: create answer comment test and use case

---
+++ src/domain/forum/application/use-cases/comment-on-anwser.spec.ts
@@ -0,0 +1,37 @@
+import { CommentAnswerUseCase } from './comment-on-anwser'
+import { InMemoryAnswerRepository } from 'test/repositories/in-memory-answer-repositorie'
+import { InMemoryAnswerCommentRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { makeAnswer } from 'test/factories/make-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswerRepository
+let inMemoryAnswerCommentRepository: InMemoryAnswerCommentRepository
+let sut: CommentAnswerUseCase
+
+describe('Create Comment on Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswerRepository()
+    inMemoryAnswerCommentRepository = new InMemoryAnswerCommentRepository()
+    sut = new CommentAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentRepository,
+    )
+  })
+
+  it('should be able create a new comment answer', async () => {
+    const answer = makeAnswer({})
+
+    await inMemoryAnswersRepository.create(answer)
+
+    const { answerComment } = await sut.execute({
+      content: 'Comentário teste',
+      authorId: answer.authorId.toString(),
+      answerId: answer.id.toString(),
+    })
+
+    expect(answerComment.id).toBeTruthy()
+    expect(inMemoryAnswerCommentRepository.items[0].content).toEqual(
+      'Comentário teste',
+    )
+  })
+})
---
+++ src/domain/forum/application/use-cases/comment-on-anwser.ts
@@ -0,0 +1,45 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+import { AnswerRepository } from '../repositories/answer-repository'
+
+interface CommentAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+interface CommentAnswerUseCaseResponse {
+  answerComment: AnswerComment
+}
+
+export class CommentAnswerUseCase {
+  constructor(
+    private answersRepository: AnswerRepository,
+    private answerCommentsRepository: AnswerCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    content,
+    answerId,
+  }: CommentAnswerUseCaseRequest): Promise<CommentAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityID(authorId),
+      content,
+      answerId: new UniqueEntityID(answerId),
+    })
+
+    await this.answerCommentsRepository.create(answerComment)
+
+    return {
+      answerComment,
+    }
+  }
+}
